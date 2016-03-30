cc841faeeff8a85152b99d324ce316ee76846deb
Sendy Prayogo <sendyprayogo04@gmail.com>
2016-03-30 10:55:15 +0700

    Update JurnalSendy.java

--- JurnalSendy.java
+++ JurnalSendy.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class JurnalSendy {
     public static void main(String[] args) {
         // TODO code application logic here
+        int i;
         System.out.print("Masukan angka : ");
         int angka = new Scanner(System.in).nextInt();
         System.out.println("\n\nAngka fibbonaci hanya sampai " + angka +" numbers : ");
-        for(int i=1; i<=angka; i++){
+        for(i=1; i<=angka; i++){
             System.out.print(pibo(i) +" ");
         }
+        int yui=i+i;
+        System.out.println("\n"+yui);
     }
     public static int pibo(int angka){
         if(angka == 1 || angka == 2){
@@ -25,11 +28,11 @@ public class JurnalSendy {
         }
         int fibo1=1, fibo2=1, fibonacci=1;
         for(int i= 3; i<= number; i++){
-            fibonacci = fibo1 + fibo2; //Fibonacci number is sum of previous two Fibonacci number
+            fibonacci = fibo1 + fibo2;
             fibo1 = fibo2;
             fibo2 = fibonacci;
  
         }
-        return fibonacci; //Fibonacci number
+        return fibonacci;
     }     
 }
